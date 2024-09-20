4115edce4a487791b7b6fc5b80b5718ae8c499ab
honggang.liu <honggang.liu@shopee.com>
2024-09-20 16:09:28 +0800

    init

--- src/main/java/com/tc/gscheduleradmin/GschedulerAdminApplication.java
+++ src/main/java/com/tc/gscheduleradmin/GschedulerAdminApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GschedulerAdminApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(GschedulerAdminApplication.class, args);
 	}
