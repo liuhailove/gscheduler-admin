070aaaf12ab527cc3d69ea66bb5ea77d8051da48
honggang.liu <honggang.liu@shopee.com>
2024-09-24 13:13:43 +0800

    init

--- src/main/java/com/tc/gscheduleradmin/GschedulerAdminApplication.java
+++ src/main/java/com/tc/gscheduleradmin/GschedulerAdminApplication.java
@@ -3,10 +3,8 @@ package com.tc.gscheduleradmin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = "com.tc")
-@EnableSwagger2
 @EnableCaching
 public class GschedulerAdminApplication {
 
